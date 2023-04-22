d1473980b420f99147b0e40fe3813e6f9dd7a821
Abhiyan <ashrestha40@myseneca.ca>
2023-04-22 17:30:20 +0000

    no more bug final push

--- modules/aws_instances/install_webserver.sh
+++ modules/aws_instances/install_webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  sudo yum -y update
  sudo yum -y install httpd
- sudo aws s3 cp s3://group19/group19matiABHI.jpeg  /var/www/html/image
+ sudo aws s3 cp s3://group19proj/group19matiABHI.jpeg  /var/www/html/image
  myip=`hostname -I`
  echo "<h1>Welcome to FInal Project ! private IP is $myip in dev environment </h1> <h2> Name: MAtiRam and Abhiyan and   </h2><br>Built by Terraform! </br>"  >  /var/www/html/index.html
  echo " <img src='image'/>" >>  /var/www/html/index.html
